1860b72890b52f6932bbb04e39c1367c0ba9e02e
sergunio <sergun811@gmail.com>
2022-05-01 23:04:33 +0300

    feat: solve 2 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  function dateSample(sampleActivity) {
+    const digitsSampleActivity = Number(sampleActivity);
+    if (!digitsSampleActivity || typeof(sampleActivity) !== 'string' || digitsSampleActivity < 0 || digitsSampleActivity > 15){
+      return false;
+      }
+      return Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/(0.693/HALF_LIFE_PERIOD));
+      
+  }
+
 
 module.exports = {
   dateSample
