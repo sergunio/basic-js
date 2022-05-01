6da8fa271a600e5bf890e94876139cb0583392dd
sergunio <sergun811@gmail.com>
2022-05-01 23:37:39 +0300

    feat: add tasks

--- src/mac-address.js
+++ src/mac-address.js
@@ -15,8 +15,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
 }
 module.exports = {
   isMAC48Address
