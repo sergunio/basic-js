60c37f6299f724f2bd34c842462354b871d39d10
sergunio <sergun811@gmail.com>
2022-05-03 02:27:38 +0300

    fix: coplete task

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(!date){
+    return 'Unable to determine the time of year!';
+  }
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0) {
+    throw new Error('Invalid date!');
+  }
+  let season;
+
+  
+  if (date.getMonth() > 1 && date.getMonth() < 5) {
+    season = 'spring';
+  } else if (date.getMonth() > 4 && date.getMonth() < 8) {
+    season = 'summer';
+  } else if (date.getMonth() > 7 && date.getMonth() < 11) {
+    season = 'autumn';
+  } else {
+    season = 'winter';
+  }
+
+  return season;
 }
 
 module.exports = {
